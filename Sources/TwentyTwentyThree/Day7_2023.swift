6a36f24b8cfb1655c5f2e9482ade2674cc215597
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2023-12-07 13:18:46 +0000

    Tidy up day 7

--- Sources/TwentyTwentyThree/Day7_2023.swift
+++ Sources/TwentyTwentyThree/Day7_2023.swift
@@ -19,37 +19,8 @@ public struct Day7 {
         case four = 4
         case three = 3
         case two = 2
-        case one = 1
+        case joker = 1
         
-        init(_ card2: Card2) {
-            self = switch card2 {
-            case .Ace: .Ace
-            case .King:
-                    .King
-            case .Queen:
-                    .Queen
-            case .Joker:
-                    .one
-            case .ten:
-                    .ten
-            case .nine:
-                    .nine
-            case .eight:
-                    .eight
-            case .seven:
-                    .seven
-            case .six:
-                    .six
-            case .five:
-                    .five
-            case .four:
-                    .four
-            case .three:
-                    .three
-            case .two:
-                    .two
-            }
-        }
         
         init(_ string: Character) {
             self = switch string {
@@ -66,42 +37,7 @@ public struct Day7 {
             case "4": .four
             case "3": .three
             case "2": .two
-            default: fatalError()
-                
-            }
-        }
-    }
-    
-    enum Card2: Int {
-        case Ace = 14
-        case King = 13
-        case Queen = 12
-        case Joker = 1
-        case ten = 10
-        case nine = 9
-        case eight = 8
-        case seven = 7
-        case six = 6
-        case five = 5
-        case four = 4
-        case three = 3
-        case two = 2
-        
-        init(_ string: Character) {
-            self = switch string {
-            case "A": .Ace
-            case "K": .King
-            case "Q": .Queen
-            case "J": .Joker
-            case "T": .ten
-            case "9": .nine
-            case "8": .eight
-            case "7": .seven
-            case "6": .six
-            case "5": .five
-            case "4": .four
-            case "3": .three
-            case "2": .two
+            case "*": .joker
             default: fatalError()
                 
             }
@@ -119,41 +55,43 @@ public struct Day7 {
     }
     
     public static func getAnswerPart1(input: [String]) throws -> Int {
-        let sortedHands = input
+        solve(for: input, handType: handType(_:))
+    }
+    
+    private static func solve(for rows: [String], handType: @escaping ([Card]) -> Hand) -> Int {
+        rows
             .map { row in
-            let rowComponents = row.components(separatedBy: " ")
-            let cards = rowComponents.first!.compactMap { Card($0) }
-            return (cards, handType(from: cards), ( Int(rowComponents.last!)!))
-        }
-            .sorted { (array1, array2) -> Bool in
-            if array1.1.rawValue > array2.1.rawValue {
-                return false
+                let rowComponents = row.components(separatedBy: " ")
+                let cards = rowComponents.first!.compactMap { Card($0) }
+                return (cards, handType(cards), ( Int(rowComponents.last!)!))
             }
-            
-            if array1.1.rawValue < array2.1.rawValue {
+            .sorted { (array1, array2) -> Bool in
+                if array1.1.rawValue > array2.1.rawValue {
+                    return false
+                }
+                
+                if array1.1.rawValue < array2.1.rawValue {
+                    return true
+                }
+                
+                let hand1Cards = array1.0
+                let hand2Cards = array2.0
+                
+                for (value1, value2) in zip(hand1Cards, hand2Cards) {
+                    if value1 != value2 {
+                        return value1.rawValue < value2.rawValue
+                    }
+                }
                 return true
             }
-            
-            let hand1Cards = array1.0
-            let hand2Cards = array2.0
-            
-            for (value1, value2) in zip(hand1Cards, hand2Cards) {
-                if value1 != value2 {
-                    return value1.rawValue < value2.rawValue
-                }
-            }
-            return true
-        }
-         
-        return sortedHands.enumerated().map { (index, handAndBet) in
+            .enumerated().map { (index, handAndBet) in
                 handAndBet.2 * (index + 1)
                 
             }
             .reduce(0, +)
-
     }
     
-    private static func handType(from cards: [Card] ) -> Hand {
+    private static func handType(_ cards: [Card] ) -> Hand {
         let setCards = Set(cards)
         let setCount = setCards.count
         
@@ -188,11 +126,11 @@ public struct Day7 {
         return .twoPair
     }
     
-    private static func handType(from jokerCards: [Card2] ) -> Hand {
+    private static func handTypeWithJokers(_ jokerCards: [Card] ) -> Hand {
         let setCards = Set(jokerCards)
         let setCount = setCards.count
         
-        if setCards.contains(.Joker) {
+        if setCards.contains(.joker) {
             if setCount == 1 {
                 return .fiveOfAKind
             }
@@ -200,7 +138,7 @@ public struct Day7 {
             var newHand: Hand = .highCard
             
             for card in setCards {
-                let hand = handType(from: jokerCards.replacing([.Joker], with: [card]).map { Card($0) })
+                let hand = handType(jokerCards.replacing([.joker], with: [card]))
                 if hand.rawValue > newHand.rawValue {
                     newHand = hand
                 }
@@ -209,42 +147,16 @@ public struct Day7 {
             return newHand
         }
         
-        return handType(from: jokerCards.map{ Card($0) })
+        return handType(jokerCards)
     }
     
     public static func getAnswerPart2(input: [String]) throws -> Int {
-        let sortedHands = input
-            .map { row in
-            let rowComponents = row.components(separatedBy: " ")
-            let cards = rowComponents.first!.compactMap { Card2($0) }
-            return (cards, handType(from: cards), ( Int(rowComponents.last!)!))
+        
+        let fixedInput = input.map { row in
+            row.replacingOccurrences(of: "J", with: "*")
         }
-            .sorted { (array1, array2) -> Bool in
-            if array1.1.rawValue > array2.1.rawValue {
-                return false
-            }
-            
-            if array1.1.rawValue < array2.1.rawValue {
-                return true
-            }
-            
-            let hand1Cards = array1.0
-            let hand2Cards = array2.0
-            
-            for (value1, value2) in zip(hand1Cards, hand2Cards) {
-                if value1 != value2 {
-                    return value1.rawValue < value2.rawValue
-                }
-            }
-            return true
-        }
-         
-        return sortedHands.enumerated().map { (index, handAndBet) in
-                handAndBet.2 * (index + 1)
-                
-            }
-            .reduce(0, +)
-
+        return solve(for: fixedInput, handType: handTypeWithJokers(_:))
+        
     }
 }
 
